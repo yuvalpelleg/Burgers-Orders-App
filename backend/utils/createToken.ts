5e6a320223c77caa371575638d4557a68c934c2b
omerlen312 <omerlen1000@gmail.com>
2025-08-21 14:38:22 +0300

    added error to secret key

--- backend/utils/createToken.ts
+++ backend/utils/createToken.ts
@@ -3,6 +3,10 @@ import jwt from "jsonwebtoken";
 const secretKey = process.env.JWT_SECRET;
 
 export function createToken(userEmail: string) {
+  if (!secretKey) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+
   const payload = {
     email: userEmail,
   };
