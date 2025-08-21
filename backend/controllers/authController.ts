da17dadcda925e521e036f6071f1ae6174fcaa55
omerlen312 <omerlen1000@gmail.com>
2025-08-21 09:49:06 +0300

    auth work in progress

---
+++ backend/controllers/authController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { User } from "../models/User";
+import { z } from "zod";
+
+const registerSchema = z.object({
+  name: z
+    .string()
+    .min(1, "Name is required")
+    .max(30, "Name must be less than 30 characters")
+    .trim(),
+
+  email: z.email("Please provide a valid email"),
+
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .max(30, "Password must be less than 30 characters")
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
+      "Password must contain at least one lowercase letter, one uppercase letter, and one number"
+    ),
+  role: z.enum(["user", "admin"]).default("user"),
+  phone: z
+    .string()
+    .regex(/^\+?[\d\s-()]+$/, "Please provide a valid phone number")
+    .optional(),
+
+  address: z.object({
+    street: z
+      .string()
+      .min(1, "Street is required")
+      .max(50, "Street must be less than 50 characters")
+      .trim(),
+    houseNumber: z.number("House number must be a number"),
+    floorNumber: z.number("Floor number must be a number").optional(),
+    apartmentNumber: z.number("Apartment number must be a number").optional(),
+  }),
+});
---
+++ backend/routes/authRoutes.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+// Public routes
+router.post("/register");
+router.post("/login");
+
+//Private Routes
+
+//router.use(authenticate); to protect all these routes
+
+router.get("/profile");
