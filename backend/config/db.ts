208bdb0be8808de0f12f590a0a993369e6250c65
yuvalpelleg <yuvalplg@gmail.com>
2025-08-21 09:44:13 +0300

    database created

---
+++ backend/config/db.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://localhost:27017/burgerApp";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
